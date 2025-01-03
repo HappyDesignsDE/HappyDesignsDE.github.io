2cf854905b5332ac6373af44a96dea22982aa11d
HappyDesignsDE <uniemail.fuer.alex@gmail.com>
2025-01-03 15:14:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Dynamically display a pop-up or message on the "Shop Now" button click
+    const shopButton = document.getElementById('shop-now');
+    
+    shopButton.addEventListener('click', function () {
+        alert('Thank you for choosing HappyDesignsDE! Visit our shop soon!');
+    });
+    
+});
